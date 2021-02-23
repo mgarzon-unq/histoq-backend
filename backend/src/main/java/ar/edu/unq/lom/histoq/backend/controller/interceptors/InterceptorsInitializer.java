32050646c3822b219386e6815798de578f6541be
Marcelo Garzon <marcelo.garzon@gmail.com>
2021-02-22 22:18:26 -0300

    Se explicitan metodos habilitados por CORS...

--- backend/src/main/java/ar/edu/unq/lom/histoq/backend/controller/interceptors/InterceptorsInitializer.java
+++ backend/src/main/java/ar/edu/unq/lom/histoq/backend/controller/interceptors/InterceptorsInitializer.java
@@ -54,6 +54,7 @@ public class InterceptorsInitializer implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowedMethods("GET","POST", "PUT", "DELETE", "OPTIONS")
                 .allowedOrigins(this.applicationConfigProperties.getCorsAllowedOrigins().split(","))
                 ;
     }
